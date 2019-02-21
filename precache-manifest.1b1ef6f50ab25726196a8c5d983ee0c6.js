647c047039572380d4efedf0a70d63d846b44f52
WereDiggle <archdiggle@gmail.com>
2019-02-20 22:20:48 -0500

    Updates

---
+++ precache-manifest.1b1ef6f50ab25726196a8c5d983ee0c6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e25a2286bd83ba40f38",
+    "url": "/CubeSpinner/static/js/runtime~main.0e25a228.js"
+  },
+  {
+    "revision": "6c7a5c0f84ac159d7932",
+    "url": "/CubeSpinner/static/js/main.6c7a5c0f.chunk.js"
+  },
+  {
+    "revision": "bf564adab7d6ea9037c7",
+    "url": "/CubeSpinner/static/js/2.bf564ada.chunk.js"
+  },
+  {
+    "revision": "8ae177351b5b9198b87244b086f54fca",
+    "url": "/CubeSpinner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.812422247e74b7f1be350e2dd495e51e.js"
+  "/CubeSpinner/precache-manifest.1b1ef6f50ab25726196a8c5d983ee0c6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CubeSpinner/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
